0c2c6036c700754d83c26ce92f68027db1ad94eb
32581649 <skywingx123@gmail.com>
2017-04-09 14:02:56 +0800

    list string piece_texture_name
    using string list only. try to change to pieces structure, with texture name and amount

---
+++ Assignment1/Assignment1/Game1.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
+
+namespace Assignment1
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch sprite_batch;
+
+        KeyboardState old_state; // for single press detection
+
+        int screen_width, screen_height;
+        DefaultPieceFormation default_formation;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            // Windows size
+            screen_width = 1024;
+            screen_height = 768;
+            graphics.PreferredBackBufferWidth = screen_width;
+            graphics.PreferredBackBufferHeight = screen_height;
+            graphics.ApplyChanges();
+
+            // Show mouse cursor
+            this.IsMouseVisible = true;
+
+            // create objects
+            default_formation = new DefaultPieceFormation();
+            
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            sprite_batch = new SpriteBatch(GraphicsDevice);
+
+            
+
+            // TODO: use this.Content to load your game content here
+            // piece
+            default_formation.Initialize(this.Content);
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+            
+            // TODO: Add your update logic here
+            KeyboardState new_state = Keyboard.GetState();
+
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            if (new_state.IsKeyDown(Keys.R))
+            {
+                if (!old_state.IsKeyDown(Keys.R))
+                {
+                    default_formation.Update(this.Content);
+                }
+            }
+
+            old_state = new_state;           
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            sprite_batch.Begin();
+            default_formation.Draw(sprite_batch);
+            sprite_batch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Assignment1/Assignment1/Piece.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace Assignment1
+{
+    class DefaultPieceFormation
+    {
+        Piece[,] pieceArray;
+        int size;
+        static List<string> piece_texture_name;
+
+        // ctor
+        public DefaultPieceFormation()
+        {
+            piece_texture_name = new List<string>();
+            // the pieces texture will appear in the default formation
+            piece_texture_name.Add("pieces/piece_wood");
+            piece_texture_name.Add("pieces/piece_redball");
+            piece_texture_name.Add("pieces/piece_greenball");
+            piece_texture_name.Add("pieces/piece_purpleball");
+            piece_texture_name.Add("pieces/piece_skyblueball");
+            piece_texture_name.Add("pieces/piece_smile");
+            piece_texture_name.Add("pieces/piece_basketball");
+
+            // default size is 8x8
+            size = 8;
+            pieceArray = new Piece[size, size];
+            for (int i = 0; i < pieceArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < pieceArray.GetLength(1); j++)
+                {
+                    pieceArray[i, j] = new Piece();
+                }
+            } 
+        }
+
+        public void Initialize(ContentManager content)
+        {
+            // randomize the pieces
+            PieceRandomize();
+
+            // starting point
+            Vector2 pos = new Vector2(200.0f, 200.0f);
+            // loop through the pieces
+            for (int i = 0; i < pieceArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < pieceArray.GetLength(1); j++)
+                {
+                    pieceArray[i, j].Initialize(content, ref pos);
+                    pos.X += 50.0f;
+                    if (j == size/2-1)
+                        pos.X += 100.0f;
+                }
+                pos.Y += 50.0f;
+                if (i == size/2-1)
+                    pos.Y += 100.0f;
+                pos.X = 200.0f;
+            }            
+        }
+
+        public void Update(ContentManager content)
+        {
+            PieceRandomize();
+            for (int i = 0; i < pieceArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < pieceArray.GetLength(1); j++)
+                {
+                    pieceArray[i, j].Update(content);
+                }
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            // loop through to draw the pieces
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    pieceArray[i, j].Draw(spriteBatch);
+                }
+            }
+        }
+
+        private void PieceRandomize()
+        {
+            Random random_int = new Random();
+            for (int i = 0; i < pieceArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < pieceArray.GetLength(1); j++)
+                {
+                    int ran = random_int.Next(0, piece_texture_name.Count);
+                    pieceArray[i,j].SetTextureName(piece_texture_name[ran]);
+                }
+            }
+        }
+    }
+    
+    class Piece
+    {
+        Texture2D piece_texture;
+        string piece_texture_name;
+        Vector2 piece_pos;
+        Vector2 scale;
+
+        public Piece()
+        {
+            piece_pos = new Vector2(0.0f, 0.0f);
+            scale = new Vector2(1.0f, 1.0f);
+            piece_texture_name = "pieces/piece_default";
+        }
+
+        public void SetTextureName(string texture_name)
+        {
+            piece_texture_name = texture_name;
+        }
+
+        public void Initialize(ContentManager content, ref Vector2 position)
+        {
+            piece_texture = content.Load<Texture2D>(piece_texture_name);
+            piece_pos = position;
+        }
+
+        public void Update(ContentManager content)
+        {
+            piece_texture = content.Load<Texture2D>(piece_texture_name);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            //spriteBatch.Draw(piece_texture, piece_pos, Color.White);
+            spriteBatch.Draw(piece_texture, piece_pos, null, Color.White, 0.0f, Vector2.Zero, scale, SpriteEffects.None, 0.0f);
+        }
+    }
+}
